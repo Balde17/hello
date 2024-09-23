3b6dcdf8101856f97126637564a8e7724d660703
balde17 <abdramanebalde4@gmail.com>
2024-09-23 04:59:45 +0000

    extract personal information to a struct

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,51 +10,54 @@ const bgImage = {
   height: "100%",
 };
 
+const personalInfo = {
+  name: "Hello, I'm Abdramane Balde",
+  job: "Web Developer | Full Stack Junior",
+  experienceDesc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque saepe dolore laudantium temporibus. Pariatur consequuntur ipsam accusamus magni qui explicabo laborum ducimus, earum at, modi temporibus placeat reiciendis aperiam quam?"
+}
 function Banner() {
   return (
-      <div id="banner"
-        style={bgImage}
-        className='min-h-[550px] sm:min-h-[600px]
+    <div id="banner"
+      style={bgImage}
+      className='min-h-[550px] sm:min-h-[600px]
         bg-gray-100 dark:bg-gray-950 dark:text-white
         duration-200 flex justify-center items-center'
-      >
-        <div className='container pb-8 sm:pb-0'>
-          <div className='grid grid-cols-1 sm:grid-cols-2'>
-            {/* text content section */}
-              <div
-              data-aos="zoom-out"
-              data-aos-duration="400"
-              data-aos-onse="true"
-              className='flex-col gap-4 justify-center text-center order-1 sm:order-2 sm:text-left'>
-                <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>Hello, I'm Abdourahmane Balde</h1>
-                <p className='text-2xl mb-3'>Web Developer | Full Stack Junior</p>
-                <p className='text-sm'>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque saepe dolore laudantium temporibus. Pariatur consequuntur ipsam accusamus magni qui explicabo laborum ducimus, earum at, modi temporibus placeat reiciendis aperiam quam?
-                </p>
-               
-              </div>
-            {/* Image section */}
-              <div className='order-1 sm:order-2 min-h-[450px] sm:min-h-[450px]
+    >
+      <div className='container pb-8 sm:pb-0'>
+        <div className='grid grid-cols-1 sm:grid-cols-2'>
+          {/* text content section */}
+          <div
+            data-aos="zoom-out"
+            data-aos-duration="400"
+            data-aos-onse="true"
+            className='flex-col gap-4 justify-center text-center order-1 sm:order-2 sm:text-left'>
+            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{personalInfo.name}</h1>
+            <p className='text-2xl mb-3'>{personalInfo.job}</p>
+            <p className='text-sm'>{personalInfo.experienceDesc}</p>
+
+          </div>
+          {/* Image section */}
+          <div className='order-1 sm:order-2 min-h-[450px] sm:min-h-[450px]
               flex justify-center items-center relative'>
-                  {/* image principale */}
-                  <div data-aos="flip-left"
-                        data-aos-duration="300"
-                        data-aos-onse="true">
-                    <img src={authorImg} alt="" 
-                      className='w-[300px] sm:w-[450px] sm:scale-125  mx-auto spin'
-                    />
-                  </div>
-                  {/* list d'image */}
-                  <div className='flex lg:flex-col lg:top-1/2
+            {/* image principale */}
+            <div data-aos="flip-left"
+              data-aos-duration="300"
+              data-aos-onse="true">
+              <img src={authorImg} alt=""
+                className='w-[300px] sm:w-[450px] sm:scale-125  mx-auto spin'
+              />
+            </div>
+            {/* list d'image */}
+            <div className='flex lg:flex-col lg:top-1/2
                   lg:-translate-y-1/2 lg:py-2
                   justify-center gap-4 absolute bottom-[0px]
                   lg:-right-10 bg-white/30 rounded-full'>
-                  
-                  </div>
-              </div>
+
+            </div>
           </div>
         </div>
       </div>
+    </div>
   )
 }
 
